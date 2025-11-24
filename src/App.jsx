7d99a1f98a2d446108b34daa4068ebc7c7d8db99
reyApolozXD <sergioeliasherreravaron@gmail.com>
2025-11-24 18:44:12 -0500

    mi primer cambio

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { baseDatos } from "./base_datos"
+
+export default function App() {
+    return (
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">nombre del activo</th>
+      <th scope="col">ubicacion</th>
+      <th scope="col">estado</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {
+      baseDatos.map(activos => (
+        <tr key={activos.id}> 
+        <td>{activos.id}</td>
+        <td>{activos.nombreActivo}</td>
+        <td>{activos.ubicacion} </td>
+        <td>{activos.estado}</td>
+      </tr>
+      )
+      )
+    }
+
+      
+
+  </tbody>
+</table>
+    )
+}
