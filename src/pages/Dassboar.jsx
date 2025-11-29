66a55114e4e88a649453ca7d435b216046c25f80
reyApolozXD <sergioeliasherreravaron@gmail.com>
2025-11-28 23:15:02 -0500

    creo que todo esta bien

--- src/pages/Dassboar.jsx
+++ src/pages/Dassboar.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import "../estilos/dassboar.css"
-import coche from "../assets/coche.png"
-import infraestructura from "../assets/infraestructura.png"
-import conexion from "../assets/conexion.png"
-import marca from "../assets/marca.png"
 import SidebarDashboard from '../component/SidebarDashboard'
 import HeaderDashboard from '../component/HeaderDashboard'
 import CardActivos from '../component/CardActivos'
@@ -18,28 +14,7 @@ export default function Dassboar() {
   return (
   <div className='cuerpo'>
     <header className="encabezado-mafis">
-      <div className="encabezado">
-        <h2 className="tuitulo -encabezado">Gestion de mantenimientos de Activos </h2>
-        <div className="nav-busqueda">
-          <form action="/buscar" method="get" className="form-buscar">
-            <input type="text" name="q" placeholder="Buscar activos, tareas, ubicaciones..." className="input-buscar" />
-            <button type="submit" className="boton-buscar"><img src={lupa} alt="buscar" width="20px" /></button>
-          </form>
-          <img src={campana} alt="campana" width="20px" height="20px" />
-        </div>
-        <div className="imagen-usuario">
-          <img className="imagen-usuario" src={perfil} alt="perfil" width="50px" />
-          <select name="opcione" id="opcione" className="opcione-encabezado">
-            <option> 
-            </option><optgroup>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option onClick={handleRetgreso}><button >Cerrar Sesión</button></option>
-            </optgroup>
-          </select>
-        </div>
-      </div> 
+      <HeaderDashboard />
       <section className="contenido-mafis">
           <CardActivos />
           <CardOrdenes />
